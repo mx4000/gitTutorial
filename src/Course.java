205f3ef9db287808898a319bee16102e4c1b7c2b
Isaac <hyf.mtu@gmail.com>
2017-02-15 12:16:04 -0500

    Add the List and the courses

---
+++ src/Course.java
@@ -0,0 +1,14 @@
+/**
+ * Created by macbook on 2017-02-15.
+ */
+
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description){
+        this.canme = cname;
+        this.description = description;
+    }
+
+}
