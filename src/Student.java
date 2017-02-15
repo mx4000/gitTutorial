93d49d4498d0ff92bd7fce2105b28adacdebc78d
Isaac <hyf.mtu@gmail.com>
2017-02-15 12:19:00 -0500

    Add the boolean fulltime

--- src/Student.java
+++ src/Student.java
@@ -2,14 +2,24 @@
  * Created by ethan on 2017-02-07.
  */
 public class Student extends Person {
+
     String major;
+    boolean fulltime;
+
+    List<Course> courseList = new List<Course>();
 
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
         this.major = major;
+        this.courseList = courseList;
+        this.fulltime = fulltime;
     }
 
     public String toString() {
         return String.format("Name: %s\nMajor: %s\nEmail: %s", this.name, this.major, this.emailAddress);
     }
+    public void addCourse(Course c){
+        courseList.add(c);
+    }
 }
+
